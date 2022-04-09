846395c157dc0adfa054521948dd10b73227a4fd
Babak <babak.jahanbani@yahoo.com>
2022-04-09 18:05:17 +0430

    Custom Validator Password MATCH

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatchPasswordsService } from 'src/app/_validators/match-passwords.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,24 +8,33 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-  form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(13),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(6),
-      Validators.maxLength(20),
-    ]),
-    passwordConfirmation: new FormControl('', [
-      Validators.required,
-      Validators.minLength(6),
-      Validators.maxLength(20),
-    ]),
-  });
-  constructor() {}
+  form = new FormGroup(
+    {
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(13),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      ]),
+      passwordConfirmation: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      ]),
+    },
+    {
+      validators: [this.matchPasswords.validate],
+    }
+  );
+  constructor(private matchPasswords: MatchPasswordsService) {}
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 
   ngOnInit(): void {}
 }
