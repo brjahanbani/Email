ee96ff0ff0ba74e6fc3a0b3e1963d29371802857
Babak <babak.jahanbani@yahoo.com>
2022-04-11 22:07:55 +0430

    Adding Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, AuthModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
