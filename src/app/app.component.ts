b6a0be805ffe212b509b4417f6e81c31263b71b6
Babak <babak.jahanbani@yahoo.com>
2022-04-12 21:56:45 +0430

    Signout Method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './_services/auth.service';
 
 @Component({
@@ -6,11 +6,18 @@ import { AuthService } from './_services/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Email';
   constructor(private authService: AuthService) {
     this.authService.checkAuth().subscribe((response) => {
       // console.log(response);
     });
+  }
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.authService.signout().subscribe((response) => {
+        console.log(response);
+      });
+    }, 5000);
   }
 }
