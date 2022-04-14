fff9c130e4df4477976a56e4785628ffbaadcd7e
Babak <babak.jahanbani@yahoo.com>
2022-04-14 13:17:31 +0430

    Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccessGuard } from './_guards/access.guard';
 
 const routes: Routes = [
   {
     path: 'inbox',
+    canLoad: [AccessGuard],
     loadChildren: () =>
       import('./inbox/inbox.module').then((m) => m.InboxModule),
   },
