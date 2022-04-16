794333c42d13374efa233a8cc9f2367d575db745
Babak <babak.jahanbani@yahoo.com>
2022-04-16 23:21:36 +0430

    rendering a list of emails

--- src/app/inbox/email-index/email-index.component.ts
+++ src/app/inbox/email-index/email-index.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailService } from 'src/app/_services/email.service';
 
 @Component({
   selector: 'app-email-index',
   templateUrl: './email-index.component.html',
-  styleUrls: ['./email-index.component.css']
+  styleUrls: ['./email-index.component.css'],
 })
 export class EmailIndexComponent implements OnInit {
-
-  constructor() { }
+  emails = [];
+  constructor(private emailService: EmailService) {}
 
   ngOnInit(): void {
+    this.emailService.getEmails().subscribe((data) => {
+      this.emails = data;
+    });
   }
-
 }
