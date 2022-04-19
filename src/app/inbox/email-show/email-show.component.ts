49ab9c816b11c20adda4aca362b3e3c2a0e8d6cb
Babak <babak.jahanbani@yahoo.com>
2022-04-19 14:50:34 +0430

    siwtch Map Operator

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 import { EmailService } from 'src/app/_services/email.service';
 
 @Component({
@@ -15,11 +16,22 @@ export class EmailShowComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(({ id }) =>
-      this.emailService.getEmail(id).subscribe((email) => {
+    this.route.params
+      .pipe(
+        switchMap(({ id }) => {
+          return this.emailService.getEmail(id);
+        })
+      )
+      .subscribe((email) => {
         this.email = email;
-      })
-    );
+      });
+
+    //**********2 subscribe is not an ideal way so we'll use  */
+    // this.route.params.subscribe(({ id }) =>
+    //   this.emailService.getEmail(id).subscribe((email) => {
+    //     this.email = email;
+    //   })
+    // );
     // console.log(this.route.snapshot.params.id);
   }
 }
