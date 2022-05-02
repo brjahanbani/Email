af0c6b87a8a402a1a3b0742f3243936f75f6bb73
Babak <babak.jahanbani@yahoo.com>
2022-05-02 12:44:54 +0430

    Setting validators  to Email Form

--- src/app/inbox/email-form/email-form.component.ts
+++ src/app/inbox/email-form/email-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { EmailDetail } from 'src/app/_services/email.service';
 
 @Component({
@@ -14,10 +14,13 @@ export class EmailFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      subject: new FormControl(this.email.subject), //reply
-      text: new FormControl(this.email.text), //reply
+      subject: new FormControl(this.email.subject, [Validators.required]), //reply
+      text: new FormControl(this.email.text, [Validators.required]), //reply
       from: new FormControl({ value: this.email.from, disabled: true }), // create  //reply
-      to: new FormControl(this.email.to), //reply
+      to: new FormControl(this.email.to, [
+        Validators.required,
+        Validators.email,
+      ]), //reply
     });
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ModalComponent } from './modal/modal.component';
+import { TextAreaComponent } from './text-area/text-area.component';
 
 @NgModule({
-  declarations: [InputComponent, ModalComponent],
+  declarations: [InputComponent, ModalComponent, TextAreaComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [InputComponent, ModalComponent],
+  exports: [InputComponent, ModalComponent, TextAreaComponent],
 })
 export class SharedModule {}
